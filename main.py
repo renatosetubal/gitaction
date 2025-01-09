24a19a3960d075d03994d197f853b02c54afa778
Renato Miranda Setúbal <remiset@gmail.com>
2025-01-09 09:50:22 -0300

    feature: Texto para a branch red

---
+++ main.py
@@ -0,0 +1,8 @@
+# Código para escrever texto na cor teal no terminal
+def write_teal(text):
+    # teal = '\033[36m'  # Código ANSI para a cor vermelho sangue
+    # reset = '\033[0m'  # Código ANSI para resetar as cores
+    print(f"{teal}{text}{reset}")
+
+# Testando a função
+write_teal("Este texto é para a branch red! - GitFlow")
